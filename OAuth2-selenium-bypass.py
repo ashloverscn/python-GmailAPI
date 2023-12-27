24eeabe0f6c41dd1dd8e37ac5eb8fa2e69d307c5
ashloverscn <ashloverscn@gmail.com>
2023-12-27 10:44:37 +0530

    Add files via upload

---
+++ OAuth2-selenium-bypass.py
@@ -0,0 +1,63 @@
+import base64
+import pandas as pd
+from random import randint, choices
+from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email import encoders
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from requests import HTTPError
+from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
+from jinja2 import Template
+import ssl
+import smtplib
+import logging
+import time as time
+import sys
+import pdfkit
+import imgkit
+import img2pdf
+import os
+import os.path
+import string
+from datetime import date
+import uuid
+import datetime
+import threading
+
+
+CLIENT_SECRETS_FILE = "./credentials/ash.temp.new@gmail.com.json"
+SCOPES = ['https://mail.google.com/']
+
+flow = InstalledAppFlow.from_client_secrets_file("./credentials/ash.temp.new@gmail.com.json", SCOPES, redirect_uri='http://localhost:8080')
+
+def run_authorization_flow():
+    cred = flow.run_local_server(port=8080, open_browser=False)
+    return cred
+
+if __name__ == '__main__':
+    
+    auth_url, _ =  flow.authorization_url(prompt='consent')
+    authorization_thread = threading.Thread(target=run_authorization_flow)
+    authorization_thread.start()
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get(auth_url)
+    print(auth_url)
+    print(auth_url)
+    print(auth_url)
+    print(auth_url)
+    
+    authorization_thread.join()
+
+    
